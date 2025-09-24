fcad837b74081a6e25db8abc5466d62aaf43b7b6
Aiden194505 <ajtowell07@gmail.com>
2025-09-24 13:09:28 -0700

    Add files via upload

---
+++ VSfile2.py
@@ -0,0 +1,14 @@
+import turtle as trtl
+myturtle = trtl.Turtle()
+
+print("Number Game")
+a=input("type a number")
+b=input("type a number")
+c=input("type a number")
+d=input("type a number")
+
+value=a+b+c+d
+print(a)
+
+wn = trtl.Screen()
+wn.mainloop()
---
+++ VsFile3loop.py
@@ -0,0 +1,17 @@
+
+N=input("first name ")
+LN=input("last name ")
+A=input("age ")
+L=input("where you live city, state ")
+P=input("How you live with. Title of relation and name ")
+
+
+
+print("Hi ", N, LN, ". You are ", A, " years old, and live in ", L, ". With ", P, ".")
+
+
+#print(N)
+#for i in "Aiden":
+    #print(i)
+
+
